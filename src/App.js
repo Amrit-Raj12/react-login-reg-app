ae7b75ddfe28b2c226d02774952d820e60b806a7
Amrit_Raj12 <amrit.raj1224@gmail.com>
2021-11-15 14:55:51 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from "./Components/Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Details from "./Components/Details";
 import Footer from "./Components/Footer";
+import Movies from "./Components/Movies";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/info" element={<Details />}/>
           <Route  path="/" element={<Register />} />
+          <Route  path="/movies" element={<Movies />} />
         </Routes>
       </Router>
       <Footer />
